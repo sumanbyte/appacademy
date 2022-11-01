c62631a8057b049c6546d14db315e64d8e136bea
sumanbyte <72149310+sumanbyte@users.noreply.github.com>
2022-11-01 15:10:58 +0545

    Long Practice: Rock Paper Scissors
    Completed refactoring rock paper scissors game. Basically got an working version of a project and implemented DRY and SRP(single responsibility principle) to make the code more readable and maintainable. There were test cases which i read and got some ideas behind them. :)

---
+++ practice-for-week-04-rock-paper-scissors-long-practice-main/game.js
@@ -0,0 +1,127 @@
+const readline = require('readline');
+
+/********************************* CONSTANTS *********************************/
+const VALID_MOVES = {
+  r: {
+    name: 'Rock',
+    winsAgainst: 's'
+  },
+  p: {
+    name: 'Paper',
+    winsAgainst: 'r'
+  },
+  s: {
+    name: 'Scissors',
+    winsAgainst: 'p'
+  }
+};
+
+/********************************* GAME DATA *********************************/
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+/* DO NOT CHANGE THE CODE ABOVE */
+
+/***************************** HELPER FUNCTIONS ******************************/
+function printHelp() {
+  // Your code here
+  console.log("  Type 'r' for Rock");
+  console.log("  Type 'p' for Paper");
+  console.log("  Type 's' for Scissors");
+  console.log("  Type 'q' to quit");
+  console.log("  Type 'h' for a list of valid commands\n");
+}
+
+function getWinner(move1, move2) {
+  // Your code here
+  if (move1 === move2) { // tie
+    console.log("You tie.\n");
+    ties++;
+    return 0
+  }
+  else if (VALID_MOVES[move1].winsAgainst === move2) { // win
+    console.log("You win!\n");
+    wins++
+    return 1
+  } else { // loss
+    console.log("You lose...\n");
+    losses++;
+    return -1
+  }
+}
+
+function getCPUMove() {
+  // Your code here
+  const validMoveKeys = Object.keys(VALID_MOVES);
+  const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
+  const cpu = validMoveKeys[randomIndex];
+
+  return cpu
+}
+
+function processMove(cmd, cpu) {
+  // Your code here
+  if(cmd === 'h'){
+    console.log("\nHelp:\n");
+    printHelp()
+  }
+  else if (VALID_MOVES[cmd]) {
+    console.log(`You pick ${cmd}, computer picks ${cpu}.`)
+    getWinner(cmd, cpu)
+    
+  }else{
+    console.log("\nInvalid command.\n");
+    printHelp()
+
+  }
+
+}
+
+/******************************* MAIN FUNCTION *******************************/
+function promptInput(rl) {
+ 
+  console.log(`${wins} wins - ${losses} losses - ${ties} ties`);
+
+
+  rl.question('> ', (cmd) => {
+    cmd = cmd.toLowerCase();
+
+    if(cmd === 'q'){
+      rl.close()
+      return
+    }
+    processMove(cmd, getCPUMove())
+
+    promptInput(rl);
+  });
+}
+
+/****************************** INITIALIZE GAME ******************************/
+function initializeGame() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  console.log("Welcome to Rock/Paper/Scissors\n");
+  printHelp()
+
+  promptInput(rl);
+}
+
+// start the game if running this file directly, `node game.js`
+// do not start the game if running test specs
+if (typeof require !== 'undefined' && require.main === module) {
+  initializeGame();
+}
+
+/**************************************************************************/
+/* DO NOT CHANGE THE CODE BELOW */
+module.exports = {
+  printHelp,
+  getWinner,
+  getCPUMove,
+  processMove,
+  promptInput
+};
