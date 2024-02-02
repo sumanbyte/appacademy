628fd56d80fb58f549f91a8f39c2146b0b896853
Suman Parajuli <72149310+sumanbyte@users.noreply.github.com>
2024-02-02 08:42:12 +0545

    Done findMinBST and findMaxBST

---
+++ 5_Data Structures and Algorithms/6_Trees_And_Binary_Search/3_Binary_Search_Tree_Projects/practice-for-week-06-binary-search-tree-long-practice/binary-search-tree.js
@@ -0,0 +1,127 @@
+// Before starting, copy and paste your guided practice work from
+// `binary-search-tree.js` into this file
+
+// Your code here
+// Do not change this
+class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class BinarySearchTree {
+  
+    constructor() {
+      // Your code here
+      this.root = null;
+    }
+  
+    insert(val, currentNode=this.root) {
+      // Your code here
+      if(currentNode == null){
+        return this.root = new TreeNode(val);
+      }
+  
+      if(currentNode.val > val && currentNode.left == null){
+        return currentNode.left = new TreeNode(val);
+      }
+      if(currentNode.val > val && currentNode.left != null){
+        return this.insert(val, currentNode.left);
+      }
+      if(currentNode.val < val && currentNode.right == null){
+        return currentNode.right = new TreeNode(val);
+      }
+      if(currentNode.val < val && currentNode.right != null){
+        return this.insert(val, currentNode.right);
+      }
+    }
+  
+    search(val) {
+      // Your code here
+      let currentNode = this.root;
+  
+      while(currentNode !== null){
+        if(val < currentNode.val){
+          currentNode = currentNode.left;
+        }else if(val > currentNode.val){
+          currentNode = currentNode.right
+        }else{
+          return true
+        }
+      }
+  
+      return false;
+    }
+  
+  
+    preOrderTraversal(currentNode = this.root) {
+      // Your code here
+      if(currentNode == null){
+        return;
+      }
+      console.log(currentNode.val);
+      this.preOrderTraversal(currentNode.left)
+      this.preOrderTraversal(currentNode.right)
+    }
+  
+  
+    inOrderTraversal(currentNode = this.root) {
+      if(currentNode === null){
+        return;
+      }
+      // Your code here
+      this.inOrderTraversal(currentNode.left);
+      console.log(currentNode.val)
+      this.inOrderTraversal(currentNode.right)
+    }
+  
+  
+    postOrderTraversal(currentNode = this.root) {
+      // Your code here
+      if(currentNode == null){
+        return;
+      }
+      this.postOrderTraversal(currentNode.left)
+      this.postOrderTraversal(currentNode.right);
+      console.log(currentNode.val);
+    }
+  
+      // Breadth First Traversal - Iterative
+    breadthFirstTraversal() {
+      // your code here
+      let queue = [];
+      queue.push(this.root);
+  
+      while(queue.length > 0){
+        const currentNode = queue.shift();
+        console.log(currentNode.val);
+        if(currentNode.left != null) queue.push(currentNode.left);
+        if(currentNode.right != null) queue.push(currentNode.right)
+      }
+      
+    }
+  
+    // Depth First Traversal - Iterative
+    depthFirstTraversal() {
+      // your code here
+  
+        //      4
+        //    /   \
+        //   2     6
+        //  / \   / \
+        // 1   3 5   7
+      const stack = [];
+      stack.push(this.root);
+      
+      while(stack.length > 0){
+        const currentNode = stack.pop();
+        console.log(currentNode.val);
+        if(currentNode.left != null) stack.push(currentNode.left)
+        if(currentNode.right != null) stack.push(currentNode.right);
+      }
+  }
+  }
+  
+  module.exports = { BinarySearchTree, TreeNode };
---
+++ 5_Data Structures and Algorithms/6_Trees_And_Binary_Search/3_Binary_Search_Tree_Projects/practice-for-week-06-binary-search-tree-long-practice/tree-practice.js
@@ -0,0 +1,105 @@
+const { BinarySearchTree, TreeNode } = require('./binary-search-tree.js');
+// Before starting, copy and paste your guided practice work into the copy
+// of `binary-search-tree.js` in this folder
+
+// Practice problems on binary trees
+
+function findMinBST (rootNode) {
+  // Your code here
+  
+
+  while(rootNode.left != null){
+    rootNode = rootNode.left
+  }
+  
+  return rootNode.val
+}
+
+function findMaxBST (rootNode) {
+  // Your code here
+  while(rootNode.right != null){
+    rootNode = rootNode.right;
+  }
+  return rootNode.val;
+}
+
+function findMinBT (rootNode) {
+  // Your code here
+}
+
+function findMaxBT (rootNode) {
+  // Your code here
+}
+
+function getHeight (rootNode) {
+  // Your code here
+}
+
+function balancedTree (rootNode) {
+  // Your code here
+}
+
+function countNodes (rootNode) {
+  // Your code here
+}
+
+function getParentNode (rootNode, target) {
+  // Your code here
+}
+
+function inOrderPredecessor (rootNode, target) {
+  // Your code here
+}
+
+function deleteNodeBST(rootNode, target) {
+  // Do a traversal to find the node. Keep track of the parent
+
+  // Undefined if the target cannot be found
+
+  // Set target based on parent
+
+  // Case 0: Zero children and no parent:
+  //   return null
+
+  // Case 1: Zero children:
+  //   Set the parent that points to it to null
+
+  // Case 2: Two children:
+  //  Set the value to its in-order predecessor, then delete the predecessor
+  //  Replace target node with the left most child on its right side, 
+  //  or the right most child on its left side.
+  //  Then delete the child that it was replaced with.
+
+  // Case 3: One child:
+  //   Make the parent point to the child
+
+}
+
+let bstRoot;
+   //      4
+    //    /   \
+    //   2     6
+    //  / \   / \
+    // 1   3 5   7
+    bstRoot = new TreeNode(4);
+    bstRoot.left = new TreeNode(2);
+    bstRoot.left.left = new TreeNode(1);
+    bstRoot.left.right = new TreeNode(3);
+    bstRoot.right = new TreeNode(6);
+    bstRoot.right.left = new TreeNode(5);
+    bstRoot.right.right = new TreeNode(7);
+
+findMinBST(bstRoot);
+
+module.exports = {
+    findMinBST,
+    findMaxBST,
+    findMinBT,
+    findMaxBT,
+    getHeight,
+    countNodes,
+    balancedTree,
+    getParentNode,
+    inOrderPredecessor,
+    deleteNodeBST
+}
